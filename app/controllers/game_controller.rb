60991b90e256a4dde74b947979f93bbe515fdfcd
stanley-world <146215090+stanley-world@users.noreply.github.com>
2023-12-01 21:44:11 +0000

    rock paper scissors

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -17,4 +17,38 @@ class GameController < ApplicationController
     render ({ :template => "game_templates/play_rock"})
   end 
 
+  def user_played_scissors
+    moves = ["rock","paper","scissors"]
+
+    @computer_move = moves.sample
+
+    if @computer_move == "scissors"
+      @outcome = "tied"
+
+    elsif @computer_move == "paper"
+      @outcome = "won"
+
+    else 
+      @outcome = "lost"
+    end 
+
+    render ({ :template => "game_templates/play_scissors"})
+  end 
+  def user_played_paper
+    moves = ["rock","paper","scissors"]
+
+    @computer_move = moves.sample
+
+    if @computer_move == "paper"
+      @outcome = "tied"
+
+    elsif @computer_move == "rock"
+      @outcome = "won"
+
+    else 
+      @outcome = "lost"
+    end 
+
+    render ({ :template => "game_templates/play_paper"})
+  end 
 end 
